723e7cae53417f79f9f93f4b834499856d90e58c
Skelrin <85646694+Skelrin@users.noreply.github.com>
2022-06-08 10:44:55 +0200

    Add class file

--- components/Model/Files.php
+++ components/Model/Files.php
@@ -2,6 +2,11 @@
 
 namespace Application\Model;
 
+require_once("components/Tools/Database/DatabaseConnection.php");
+
+use Application\Tools\Database\DatabaseConnection;
+
+
 class Files
 {
     private string $id_fichier;
@@ -26,51 +31,47 @@ class Files
 		$this->taille_Mo = $this->getFileSize($id_fichier);
 		$this->type = $this->getFileType($id_fichier);
 		$this->extension = $this->getFileExtension($id_fichier);
-		$this->supprime = $this->getBasket($id_fichier);
+		$this->supprime = false;
     }
 	
 	public function getAuthor(int $id_fichier): string
 	{
-		
+		return (new DatabaseConnection())->get_file($id_fichier)["email"];
 	}
 	
 	public function getPath(int $id_fichier): string
 	{
-		
+		$connection = new DatabaseConnection();
+		return $connection->get_file($id_fichier)["source"] . '\\\\' . $connection->get_file($id_fichier)["nom_fichier"];
 	}
 	
 	public function getFilename(int $id_fichier): string
 	{
-		
+		return (new DatabaseConnection())->get_file($id_fichier)["nom_fichier"];
 	}
 	
 	public function getReleaseDate(int $id_fichier): string
 	{
-		
+		return (new DatabaseConnection())->get_file($id_fichier)["date_publication"];
 	}
 	
 	public function getModificationDate(int $id_fichier): string
 	{
-		
+		return (new DatabaseConnection())->get_file($id_fichier)["date_derniere_modification"];
 	}
 	
-	public function getFileSize(int $id_fichier): string
+	public function getFileSize(int $id_fichier): float
 	{
-		
+		return (new DatabaseConnection())->get_file($id_fichier)["taille_Mo"];
 	}
 	
 	public function getFileType(int $id_fichier): string
 	{
-		
+		return (new DatabaseConnection())->get_file($id_fichier)["type"];
 	}
 	
 	public function getFileExtension(int $id_fichier): string
 	{
-		
-	}
-	
-	public function getBasket(int $id_fichier): string
-	{
-		
+		return (new DatabaseConnection())->get_file($id_fichier)["extension"];
 	}
 }
