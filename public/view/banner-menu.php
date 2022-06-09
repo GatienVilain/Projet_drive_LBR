7ab9fb0ee4b7d87927b561dea34fe6e0abbb342a
GatienVilain <Gatien.vilain@outlook.fr>
2022-06-09 12:18:11 +0200

    Add logout button and home path

--- public/view/banner-menu.php
+++ public/view/banner-menu.php
@@ -3,7 +3,8 @@
 
 <button title="Ouvrir la barre de navigation" id="navbar-button"></button>
 <nav id="navbar">
-    <a title="Accéder au menu principal" href="#">Home</a>
+    <a title="Se déconnecter et revenir à la page de connexion" href="index.php?action=logout">Déconnexion</a>
+    <a title="Accéder au menu principal" href="index.php">Home</a>
     <a title="Voir la page de mon profil et modifier mes informations" href="index.php?action=profile">Profil</a>
     <a title="Accéder à la corbeille, pour restaurer ou supprimer définitivement les fichiers supprimer" href="#">Corbeille</a>
     <!-- Version mobile -->
