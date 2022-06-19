069d1296ea8af50a00715b6adb07f840e3f33b75
GatienVilain <Gatien.vilain@outlook.fr>
2022-06-19 19:29:43 +0200

    remove import button on navbar in mobile version

--- public/view/banner-menu.php
+++ public/view/banner-menu.php
@@ -14,7 +14,7 @@ else { $admin_navbar = ""; } ?>
     <a title="Voir la page de mon profil et modifier mes informations" href="index.php?action=profile">Profil</a>
     <a title="Accéder à la corbeille, pour restaurer ou supprimer définitivement les fichiers supprimer" href="index.php?action=basket">Corbeille</a>
     <!-- Version mobile -->
-    <a title="Ajouter un fichiers" href="#" class="mobile">Importer</a>
+    <!-- <a title="Ajouter un fichiers" href="#" class="mobile">Importer</a> -->
 
     <!-- Pour les administrateurs uniquement -->
     <?= $admin_navbar ?>
