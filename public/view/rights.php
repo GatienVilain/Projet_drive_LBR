497190a0e45ef9ac9c43e95c8c003607559e629a
GatienVilain <Gatien.vilain@outlook.fr>
2022-06-22 13:08:05 +0200

    add change role features

--- public/view/rights.php
+++ public/view/rights.php
@@ -16,10 +16,29 @@
             <!-- remplacer par le nom du profil utilisateur -->
             <?= $name ?>
         </h3>
-        <p class="role">
-            <!-- ȑemplacer par le rôle de l’utilisateur -->
-            <?= $role ?>
-        </p>
+        <form action="index.php?action=changeRole&for=<?= $email ?>" method="post" id="change-role">
+            <select name="role">
+
+                <?php if ($role == "admin")
+                {  ?>
+
+                    <option value="admin" selected>admin</option>
+                    <option value="invite">invite</option>
+
+                    <?php
+                }
+                else {  ?>
+
+                    <option value="admin">admin</option>
+                    <option value="invite" selected>invite</option>
+
+                    <?php
+                }
+                ?>
+
+            </select>
+            <button type="submit">🖉 valider</button>
+        </form>
         <p class="date" >Inscrit depuis le
                 <!-- `Remplacer par de la date d’inscription de l’utilisateur -->
                 <?= $registration_date ?>
